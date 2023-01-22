9aa46974584012555370e73ca815d3659015bc96
Sam <iaiznura@gmail.com>
2023-01-22 20:48:50 +0300

    add func title_letter

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def upper_letter(string):
+    """Принимает на вход строку и возвращает её, со всеми заглавными буквами"""
     return string.upper()
+
+
+def title_letter(string):
+    """Делает заглавными первые буквы каждого слова в строке"""
+    return string.title()
+
+
+print(title_letter("popopsdoaspofpasof asdasd asd asd asdasd ads"))
